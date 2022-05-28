1cac52d6665a40511be1484ca77a936543e61895
krishanathep <krishanathep@gmail.com>
2022-05-28 23:16:35 +0700

    update package json

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("[success] task 1 : listening on port " + port);
